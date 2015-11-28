d25652644dad736d6d78342ffa257244bade452e
Alexander Junge <ajunge@rth.dk>
2015-11-28 19:20:07 +0100

    test mutual information computation

---
+++ tests/test_mutual_information.py
@@ -0,0 +1,15 @@
+import os
+import unittest
+import bitools.mutual_information as mi
+
+
+class TestMutualInformation(unittest.TestCase):
+    def test_compute_mutual_information(self):
+        mutual_information_file = os.path.join('tests', 'test_mutual_information.fa')
+        base_pair_count, mean_mutual_information = mi.compute_mutual_information(mutual_information_file)
+        self.assertEqual(3, base_pair_count)
+        self.assertEqual(1, mean_mutual_information)
+
+
+if __name__ == '__main__':
+    unittest.main()
